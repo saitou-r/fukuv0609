029c1b3de350c483e73b76fd34ba0295588b1cb1
A061-09 <tnk.dat.class@gmail.com>
2022-06-09 13:40:25 +0900

    復習

--- fukuv0609/Form1.cs
+++ fukuv0609/Form1.cs
@@ -17,5 +17,25 @@ namespace fukuv0609
             InitializeComponent();
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("齋藤麗也");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left = int.Parse(textBox1.Text);
+            label1.Top = int.Parse(textBox2.Text);
+        }
     }
 }
